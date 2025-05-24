93832125afad413728f04c88d33fd41bc93be887
Merl0k <92103324+Merl0k@users.noreply.github.com>
2025-05-24 17:49:41 +0300

    Add files via upload

---
+++ Laba_7/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Парні числа від 2 до 20:");
+
+        for (int i = 2; i <= 20; i += 2)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine(); 
+    }
+}
+*/
+/*
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        int number;
+
+        Console.WriteLine("Вводьте числа (введення 0 завершить цикл):");
+
+        while (true)
+        {
+            Console.Write("Введіть число: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 0)
+                break;
+
+            sum += number;
+        }
+
+        Console.WriteLine("Сума чисел: " + sum);
+    }
+}
+*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string correctPassword = "1234";
+        string input;
+
+        do
+        {
+            Console.Write("Введіть пароль: ");
+            input = Console.ReadLine();
+
+            if (input != correctPassword)
+            {
+                Console.WriteLine("Неправильний пароль!");
+            }
+
+        } while (input != correctPassword);
+
+        Console.WriteLine("Доступ дозволено!");
+    }
+}
+
