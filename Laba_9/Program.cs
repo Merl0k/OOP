5ff8aa4730038c0830d12e08c02c3984e290e15e
Merl0k <92103324+Merl0k@users.noreply.github.com>
2025-05-24 18:00:46 +0300

    Add files via upload

---
+++ Laba_9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 8, 5, 2, 9, 1, 5, 6 };
+        int swapCount = 0;
+
+        for (int i = 0; i < numbers.Length - 1; i++)
+        {
+            for (int j = 0; j < numbers.Length - 1 - i; j++)
+            {
+                if (numbers[j] > numbers[j + 1])
+                {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+
+                    swapCount++; 
+                }
+            }
+        }
+
+        Console.WriteLine($"Кількість перестановок: {swapCount}");
+        Console.Write("Після сортування: [");
+        Console.Write(string.Join(", ", numbers));
+        Console.WriteLine("]");
+    }
+}
+    
